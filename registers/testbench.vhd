85fa9ef03a5471331ccefb11026465f6191fb6b9
Dérick <120838145+derickandrade@users.noreply.github.com>
2025-01-30 13:25:12 -0300

    Update testbench.vhd

--- registers/testbench.vhd
+++ registers/testbench.vhd
@@ -37,69 +37,34 @@ begin
     ro2  => ro2_tb );
     
     process
-      begin       
-      data_tb <= X"AAAAAAAA";	-- dado para escrever
-      rd_tb   <= "00101"; 	  -- no registrador 5
-      wren_tb <= '1'; 			  -- habilita o sinal de escrita
-      wait for 1 ns;
-
-      clk_tb <= '1';			-- subida do clock
-      wait for 1 ns;
-
-      clk_tb <= '0';			-- descida do clock
-      wait for 1 ns;    
-
-      data_tb <= X"BBBBBBBB";
-      rd_tb   <= "00111";		-- escreve no reg 7
-      -- sinal de escrita continua ativo
-
-      wait for 1 ns;
-
-      clk_tb <= '1';			-- clock sobe para escrever de novo
-
-      wait for 1 ns;
-      wren_tb <= '0';			-- desativa sinal de escrita
-      rs1_tb  <= "00101";	
-      rs2_tb  <= "00111";		-- para ler os registradores 5 e 7	
-
-      wait for 1 ns;
-
-      clk_tb <= '1';
-
-      assert ro1_tb = X"AAAAAAAA" report "ro1 falhou" severity error;
-      assert ro2_tb = X"BBBBBBBB" report "ro2 falhou" severity error;
-
-      wait for 1 ns;
-
-      clk_tb  <= '0';
+      begin
       wren_tb <= '1';
-
-      wait for 1 ns;
-
-      data_tb <= X"FFFFFFFF"; -- tentativa de escrever no registrador 0
-      rd_tb   <= "00000";
-
-      wait for 1 ns;
-
-      clk_tb <= '1';
-
-      wait for 1 ns;
-
-      clk_tb  <= '0';
+      for I in 0 to 31 loop
+      	clk_tb <= '0';
+      	data_tb <= std_logic_vector(to_unsigned((I + 1), 32)); -- I + 1 para colocar algo diferente de 0 no reg zero
+        rd_tb <= std_logic_vector(to_unsigned(I, 5));
+        wait for 1 ns;
+        clk_tb <= '1';
+        wait for 1 ns;
+      end loop;
+      
       wren_tb <= '0';
-
-      wait for 1 ns;
-
-      rs1_tb  <= "00000";
-      rs2_tb  <= "00000"; -- verificação do que está guardado no reg 0
-
-      wait for 1 ns;
-
-      clk_tb <= '1';
-
-      assert ro1_tb = X"00000000" report "ro1 zero falhou" severity error;
-      assert ro2_tb = X"00000000" report "ro2 zero falhou" severity error;
-
+      for I in 0 to 31 loop
+      	clk_tb <= '0';
+        rs1_tb <= std_logic_vector(to_unsigned(I, 5));
+        rs2_tb <= std_logic_vector(to_unsigned(I, 5));
+        wait for 1 ns;
+        clk_tb <= '1';
+        if I /= 0 then
+        	assert ro1_tb = std_logic_vector(to_unsigned((I + 1), 32)) report "falhou" severity error; -- se o I for diferente de 0, testa se o valor no reg é I + 1
+        	assert ro2_tb = std_logic_vector(to_unsigned((I + 1), 32)) report "falhou" severity error;
+        else
+        	assert ro1_tb = X"00000000" report "falhou" severity error; -- senão, testa se o valor do reg zero é 0
+        	assert ro2_tb = X"00000000" report "falhou" severity error;
+        end if;
+        wait for 1 ns;
+      end loop;            
+	  
       wait;
     end process;
 end tb;
